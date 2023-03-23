bbccfa139ec8ff390de82fcfef51a30f19092306
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2023-03-23 18:46:54 +1300

    Adding all remaining compenents

--- Components/Notice Board/BoardContainer.tsx
+++ Components/Notice Board/BoardContainer.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react"; 
 import {ScrollView} from 'react-native';
 
+
+
+
+
+
+const[Post,setPost] = useState([
+    {name:'Palm Sunday'},
+    {name: 'Maundy Thursday'}
+]);
+
 const BoardContainer = () => {
-  return( 
-  <ScrollView> </ScrollView>
+    return (
+        <ScrollView>
+            
+        </ScrollView>
     )
 }
+
+export default BoardContainer;
