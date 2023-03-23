ce6424522078ecd557a0fb755562f577634b20af
eliazardan <danny@techbuddy.co.nz>
2023-03-23 17:58:32 +1300

    Update PostsContatainer.tsx
    New attributes for the Post Container

--- Components/Notice Board/PostsContatainer.tsx
+++ Components/Notice Board/PostsContatainer.tsx
@@ -1,9 +1,24 @@
 mport React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 
 const PostsContainer = () => {
   return( 
-  <View>
+  <View style={styles.msgBox}>
+    <Text>"Lets see what this text looks like</Text>
   </View>
     )
 }
+
+const styles = StyleSheet.create({
+  msgBox:{
+    backgroundColor: '#EAE7DB',
+    borderWidth: 2px,
+    borderColor: '#000',
+    padding: 4px,
+    width: 90%,
+    margin: 5px,
+    borderRadius: 3px,
+    height: 100px,
+    
+  }
+})
