aafe2758aefab51943c27cc4df61fc39a16bd395
eliazardan <danny@techbuddy.co.nz>
2023-03-29 16:46:15 +1300

    Updated with origin that previously did not appear
    Updated as per comments

--- Components/Notice Board/PostAuthorContainer.tsx
+++ Components/Notice Board/PostAuthorContainer.tsx
@@ -4,3 +4,21 @@ import React from 'react';
 // the component will be able to be passed the font settings (to be built)
 // text component will be with a view 
 // view attributes based on Peters figma design.
+
+import {View, StyleSheet, Text} from 'react-native';
+
+const PostAuthorContainer = () => {
+  return( 
+  <View style={styles.nameBox}>
+    <PostsAuthorName></PostsAuthorName>
+  </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  nameBox:{
+    width: 100px,
+    height: 23px,
+
+  }
+})
