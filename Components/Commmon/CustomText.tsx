6dd28656b0af24cc9950ef95ab925a9caec7a8c8
eliazardan <danny@techbuddy.co.nz>
2023-03-29 18:13:46 +1300

    Create custom text component
    create custom text and props to pass through text

--- Components/Commmon/CustomText.tsx
+++ Components/Commmon/CustomText.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
-
+import { StyleSheet, Text } from 'react-native/types';
 
 // should return a text component
 // component needs to be able to recieve font settings and set them.
+type Props = {
+    font-size: number,
+    font-family: string,
+    colour: string,
+    text: string
+};
+
+export const CustomText = ({
+    font-size,
+    font-family,
+    colour,
+    text
+
+}: Props) => {
+    return (
+        <Text style = {[styles.customText, {font-size, font-family, colour}]}>text</Text>
+    );
+
+}
+
+
+const styles = StyleSheet.create({
+    customText: {},
+})
